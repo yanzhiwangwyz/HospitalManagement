954d4fec22c4ddb628e4a45f5514602139cb6d03
yanzhiwangwyz <80643510+yanzhiwangwyz@users.noreply.github.com>
2023-07-15 22:31:12 +0000

    update delete patient

--- src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
+++ src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
@@ -6,8 +6,6 @@ import org.yanzhiwang.hospital.service.PatientService;
 import org.yanzhiwang.hospital.model.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.*;
 
