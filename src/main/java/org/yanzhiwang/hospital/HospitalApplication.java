4c12c6949cd1d2149472a52cb06d9b7c972affb7
yanzhiwangwyz <80643510+yanzhiwangwyz@users.noreply.github.com>
2023-07-15 15:00:34 +0000

    create UI

--- src/main/java/org/yanzhiwang/hospital/HospitalApplication.java
+++ src/main/java/org/yanzhiwang/hospital/HospitalApplication.java
@@ -1,7 +1,5 @@
 package org.yanzhiwang.hospital;
 
-import javax.print.Doc;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
+++ src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
@@ -1,25 +1,30 @@
 package org.yanzhiwang.hospital.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.yanzhiwang.hospital.service.PatientService;
 import org.yanzhiwang.hospital.model.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.*;
 
+@Controller
 public class PatientController {
     @Autowired
     private PatientService patientService;
 
-    public String displayAllPatients(Model model) {
-        List<Patient> patients = patientService.getAllPatients();
-        model.addAttribute("patients", patients);
-        return "patients";
-    }
-    // public ModelAndView displayAllPatients() {
-    // List<Patient> patients = patientService.getAllPatients();
-    // ModelAndView mav = new ModelAndView("patients");
-    // mav.addObject("patients", patients);
-    // return mav;
+    // @GetMapping("/patients")
+    // public String displayAllPatients(Model model) {
+    //     List<Patient> patients = patientService.getAllPatients();
+    //     model.addAttribute("patients", patients);
+    //     return "patients";
     // }
+    @GetMapping("/patients")
+    public ModelAndView displayAllPatients() {
+    List<Patient> patients = patientService.getAllPatients();
+    ModelAndView mav = new ModelAndView("patients");
+    mav.addObject("patients", patients);
+    return mav;
+    }
 }
