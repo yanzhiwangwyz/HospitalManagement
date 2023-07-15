96662064daf699e06df3f98b3bd68d9106276b62
yanzhiwangwyz <80643510+yanzhiwangwyz@users.noreply.github.com>
2023-07-15 23:20:42 +0000

    update delete patient

--- src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
+++ src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
@@ -6,6 +6,7 @@ import org.yanzhiwang.hospital.service.PatientService;
 import org.yanzhiwang.hospital.model.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.*;
 
@@ -36,7 +37,7 @@ public class PatientController {
         ModelAndView mav = new ModelAndView("patients_delete_confirmation_page");
         mav.addObject("patientObject", patientObject);
         return mav;
-    } 
+    }
 
     @GetMapping("/remove-patient/{id}")
     public ModelAndView removePatient(@PathVariable("id") Long id) {
@@ -45,4 +46,14 @@ public class PatientController {
         mav.addObject("patientList", patientService.getAllPatients());
         return mav;
     }
+
+    // @PostMapping("/remove-patient/{id}")
+    // public ModelAndView removePatient(@PathVariable("id") Long id) {
+    // patientService.deletePatientById(id);
+    // ModelAndView mav = new ModelAndView("patients_page");
+    // mav.addObject("patientList", patientService.getAllPatients());
+    // return mav;
+    // } 
+
+    
 }
