4bae95fb53b0e706b3d8c0072ddcc90daa5381c2
yanzhiwangwyz <80643510+yanzhiwangwyz@users.noreply.github.com>
2023-07-16 02:55:48 +0000

    create update UI

--- src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
+++ src/main/java/org/yanzhiwang/hospital/controller/PatientController.java
@@ -2,11 +2,10 @@ package org.yanzhiwang.hospital.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.yanzhiwang.hospital.service.PatientService;
+import org.yanzhiwang.hospital.service.*;
 import org.yanzhiwang.hospital.model.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.*;
 
@@ -14,11 +13,17 @@ import java.util.*;
 public class PatientController {
     @Autowired
     private PatientService patientService;
+    @Autowired
+    private DoctorService doctorService;
+    @Autowired
+    private DepartmentService departmentService;
+    @Autowired
+    private HospitalService hospitalService;
 
     @GetMapping("/patients")
     public ModelAndView displayAllPatients() {
         List<Patient> patientList = patientService.getAllPatients();
-        ModelAndView mav = new ModelAndView("patients_page");
+        ModelAndView mav = new ModelAndView("patientsPage");
         mav.addObject("patientList", patientList);
         return mav;
     }
@@ -26,7 +31,7 @@ public class PatientController {
     @GetMapping("/patient-profile/{id}")
     public ModelAndView displayPatientById(@PathVariable("id") Long id) {
         Patient patientObject = patientService.getPatientById(id);
-        ModelAndView mav = new ModelAndView("patient_profile_page");
+        ModelAndView mav = new ModelAndView("patientProfilePage");
         mav.addObject("patientObject", patientObject);
         return mav;
     }
@@ -34,7 +39,7 @@ public class PatientController {
     @GetMapping("/remove-patient-confirm/{id}")
     public ModelAndView removePatientConfirmation(@PathVariable("id") Long id) {
         Patient patientObject = patientService.getPatientById(id);
-        ModelAndView mav = new ModelAndView("patients_delete_confirmation_page");
+        ModelAndView mav = new ModelAndView("patientsDeleteConfirmationPage");
         mav.addObject("patientObject", patientObject);
         return mav;
     }
@@ -42,7 +47,7 @@ public class PatientController {
     @GetMapping("/remove-patient/{id}")
     public ModelAndView removePatient(@PathVariable("id") Long id) {
         patientService.deletePatientById(id);
-        ModelAndView mav = new ModelAndView("patients_page");
+        ModelAndView mav = new ModelAndView("patientsPage");
         mav.addObject("patientList", patientService.getAllPatients());
         return mav;
     }
@@ -53,7 +58,16 @@ public class PatientController {
     // ModelAndView mav = new ModelAndView("patients_page");
     // mav.addObject("patientList", patientService.getAllPatients());
     // return mav;
-    // } 
+    // }
 
-    
+    @GetMapping("/update-patient/{id}")
+    public ModelAndView updatePatient(@PathVariable("id") Long id) {
+        Patient patientObject = patientService.getPatientById(id);
+        ModelAndView mav = new ModelAndView("patientUpdatePage");
+        mav.addObject("patientObject", patientObject);
+        mav.addObject("doctorList", doctorService.getAllDoctors());
+        mav.addObject("departmentList", departmentService.getAllDepartments());
+        mav.addObject("hospitalList", hospitalService.getAllHospitals());
+        return mav;
+    }
 }
