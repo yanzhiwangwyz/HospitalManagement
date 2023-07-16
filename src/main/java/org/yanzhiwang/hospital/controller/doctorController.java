247333216d9190181937d86fcf5aef4569b4b511
yanzhiwangwyz <80643510+yanzhiwangwyz@users.noreply.github.com>
2023-07-16 19:24:37 +0000

    create add doctor UI

--- src/main/java/org/yanzhiwang/hospital/controller/doctorController.java
+++ src/main/java/org/yanzhiwang/hospital/controller/doctorController.java
@@ -38,7 +38,36 @@ public class doctorController {
         ModelAndView mav = new ModelAndView("doctorsPage");
         mav.addObject("doctorList", doctorService.getAllDoctors());
         return mav;
-    } 
+    }
 
-    
+    @GetMapping("/update-doctor/{id}")
+    public ModelAndView updateDoctor(@PathVariable("id") Long id) {
+        Doctor doctorObject = doctorService.getDoctorById(id);
+        ModelAndView mav = new ModelAndView("doctorUpdatePage");
+        mav.addObject("doctorObject", doctorObject);
+        return mav;
+    }
+
+    @PostMapping("/save-doctor-update/{id}")
+    public ModelAndView saveDoctorUpdate(@PathVariable("id") Long id, Doctor updatedDoctor) {
+        // Get the existing doctor by ID
+        Doctor existingDoctor = doctorService.getDoctorById(id);
+        // Update the fields of the existing doctor with the new values
+        existingDoctor.setName(updatedDoctor.getName());
+        // Update other fields as needed
+
+        // Save the updated doctor
+        doctorService.updateDoctor(existingDoctor);
+
+        ModelAndView mav = new ModelAndView("doctorsPage");
+        mav.addObject("doctorList", doctorService.getAllDoctors());
+        return mav;
+    }
+
+    @GetMapping("/add-doctor")
+    public ModelAndView addDoctor(Doctor doctorObject) {
+        ModelAndView mav = new ModelAndView("doctorAddPage");
+        mav.addObject("doctorObject", doctorObject);
+        return mav;
+    }
 }
